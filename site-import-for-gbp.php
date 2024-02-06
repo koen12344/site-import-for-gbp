35448b71ca9ea3895827fa841d3482d922881c93
Koen Reus <koen@koenreus.com>
2024-02-06 19:41:59 +0100

    first commit

---
+++ site-import-for-gbp.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Site Import for Google Business Profile (Google My Business)
+ * Plugin URI:      https://tycoonmedia.net
+ * Description:     Site Import for GBP will import your Google My Business posts, images, reviews and other data into your WordPress website
+ * Author:          Koen Reus
+ * Author URI:      https://koenreus.com
+ * Text Domain:     site-import-for-gbp
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Site_Import_For_Gbp
+ */
+
+// Your code starts here.
---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Site_Import_For_Gbp
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
